3ca6219a72298a375739292a607065fe8123d4af
xayuzaii <wizardcodingmachine@gmail.com>
2024-09-11 16:33:43 -0600

    added shadcn and refactored header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,13 @@
+import Header from "@/app/components/Header";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <main>
+    <main className="h-screen overflow-y-scroll bg-gradient-to-b from-slate-800 to-slate-700">
+      <Header />
       {children}
     </main>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,6 @@
-import Header from "@/app/components/Header";
 
 export default function Home() {
   return (
-    <div className="">
-      <Header/>
-    </div>
+    <></>
   );
 }
